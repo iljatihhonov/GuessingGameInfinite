320ed8ca02857ae3f24fe66ea4ab15576a96cf80
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-04-14 14:15:13 +0300

    Guess a number!

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandom = rnd.Next(1, 11);
+            int answer = 11;
+            Console.WriteLine("Try to guess my number! It is from 0 to 10 only!");
+           
+            while (answer != myRandom)
+            {
+                
+                answer = Convert.ToInt32(Console.ReadLine());
+                if (answer == myRandom)
+                {
+                    Console.WriteLine($"You are right, the answer is {answer}!");
+                
+                }
+                else
+                {
+                    Console.WriteLine("WRONG! Just try again!");
+                }
+            }          
+
+
+        }
+
+    }
+}
